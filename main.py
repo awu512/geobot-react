36fcc5efaad6bad5901698c02772b4e602327955
Jan Charatan <jan.charatan@gmail.com>
2023-03-14 18:40:35 -0700

    Add template lightningmodule from pytorch lightning getting started

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
+import os
+
 import hydra
+import pytorch_lightning as pl
 from jaxtyping import install_import_hook
 from omegaconf import DictConfig
+from torch import nn, optim, utils
+from torchvision.datasets import MNIST
+from torchvision.transforms import ToTensor
+
 
 with install_import_hook("foo", "beartype.beartype"):
-    pass
+    from src.MainLightningModule import MainLightningModule
 
 
 @hydra.main(version_base=None, config_path="configs", config_name="main")
 def main(cfg: DictConfig):
-    pass
+    dataset = MNIST(os.getcwd(), download=True, transform=ToTensor())
+    train_loader = utils.data.DataLoader(dataset)
+
+    autoencoder = MainLightningModule()
+    trainer = pl.Trainer(limit_train_batches=100, max_epochs=1)
+    trainer.fit(model=autoencoder, train_dataloaders=train_loader)
 
 
 if __name__ == "__main__":
