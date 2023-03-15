5ed0e074cde4d1c532ef0a0e059290f8f5d13264
Jan Charatan <jan.charatan@gmail.com>
2023-03-14 18:34:22 -0700

    Start setting up jaxtyping and ruff

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import hydra
+from jaxtyping import install_import_hook
 from omegaconf import DictConfig
+
+with install_import_hook("foo", "beartype.beartype"):
+    pass
 
 
 @hydra.main(version_base=None, config_path="configs", config_name="main")
 def main(cfg: DictConfig):
-    a = 1
+    pass
 
 
 if __name__ == "__main__":
