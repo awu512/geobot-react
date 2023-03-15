267787f70fea935e2dda8d6b4dab3fcbf3f3016d
Jan Charatan <jan.charatan@gmail.com>
2023-03-14 18:27:52 -0700

    Add hydra configs.

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+import hydra
+from omegaconf import DictConfig
+
+
+@hydra.main(version_base=None, config_path="configs", config_name="main")
+def main(cfg: DictConfig):
+    a = 1
+
+
 if __name__ == "__main__":
-    print("hello")
+    main()
