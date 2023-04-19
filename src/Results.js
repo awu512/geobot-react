381e7221d8e3a2f05b14f1221eade8e4cb4410e8
aidan <aid1@bamba.us>
2023-04-18 20:59:29 -0700

    switch test url for user uploaded

--- src/Results.js
+++ src/Results.js
@@ -1,14 +1,14 @@
 import './Results.css';
 import Ranking from './Ranking';
 
-function Results() {
+function Results({image}) {
     return (
         <div className="results">
             <div className='img-cont'>
                 <img 
                     className="img" 
                     alt="" 
-                    src='https://images.unsplash.com/photo-1553061599-86c4cfa449d9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MnwxMDE5NDU4fHxlbnwwfHx8fA%3D%3D&w=1000&q=80'
+                    src={URL.createObjectURL(image)}
                 />
             </div>
             <Ranking
